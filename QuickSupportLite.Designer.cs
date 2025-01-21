40413ebed1e6477fc16e97ca497d8a5f01c93a54
Joshua <aboy336@users.noreply.github.com>
2025-01-20 23:37:43 -0500

    Add project files.

---
+++ QuickSupportLite.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace QuickSupportLite
+{
+    partial class QuickSupportLite
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bafButton = new System.Windows.Forms.Button();
+            this.clupButton = new System.Windows.Forms.Button();
+            this.apciFixButton = new System.Windows.Forms.Button();
+            this.wafButton = new System.Windows.Forms.Button();
+            this.rootElementButton = new System.Windows.Forms.Button();
+            this.baldoLocalButton = new System.Windows.Forms.Button();
+            this.logFolderButton = new System.Windows.Forms.Button();
+            this.apciFolderButton = new System.Windows.Forms.Button();
+            this.appwizButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.fidoStatusIcon = new System.Windows.Forms.PictureBox();
+            this.clupStatusIcon = new System.Windows.Forms.PictureBox();
+            this.serviceStatusIcon = new System.Windows.Forms.PictureBox();
+            this.fidoButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.servicesButton = new System.Windows.Forms.Button();
+            this.systemInfoButton = new System.Windows.Forms.Button();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.serviceStatusTimer = new System.Windows.Forms.Timer(this.components);
+            this.clupStatusTimer = new System.Windows.Forms.Timer(this.components);
+            this.fidoStatusTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.fidoStatusIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clupStatusIcon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.serviceStatusIcon)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // bafButton
+            // 
+            this.bafButton.Location = new System.Drawing.Point(9, 151);
+            this.bafButton.Name = "bafButton";
+            this.bafButton.Size = new System.Drawing.Size(164, 63);
+            this.bafButton.TabIndex = 0;
+            this.bafButton.Text = "BAF";
+            this.bafButton.UseVisualStyleBackColor = true;
+            // 
+            // clupButton
+            // 
+            this.clupButton.Location = new System.Drawing.Point(179, 151);
+            this.clupButton.Name = "clupButton";
+            this.clupButton.Size = new System.Drawing.Size(164, 63);
+            this.clupButton.TabIndex = 1;
+            this.clupButton.Text = "CLUP";
+            this.clupButton.UseVisualStyleBackColor = true;
+            // 
+            // apciFixButton
+            // 
+            this.apciFixButton.Location = new System.Drawing.Point(6, 45);
+            this.apciFixButton.Name = "apciFixButton";
+            this.apciFixButton.Size = new System.Drawing.Size(164, 63);
+            this.apciFixButton.TabIndex = 3;
+            this.apciFixButton.Text = "APCI";
+            this.apciFixButton.UseVisualStyleBackColor = true;
+            // 
+            // wafButton
+            // 
+            this.wafButton.Location = new System.Drawing.Point(176, 45);
+            this.wafButton.Name = "wafButton";
+            this.wafButton.Size = new System.Drawing.Size(164, 63);
+            this.wafButton.TabIndex = 4;
+            this.wafButton.Text = "WAF";
+            this.wafButton.UseVisualStyleBackColor = true;
+            // 
+            // rootElementButton
+            // 
+            this.rootElementButton.Location = new System.Drawing.Point(346, 45);
+            this.rootElementButton.Name = "rootElementButton";
+            this.rootElementButton.Size = new System.Drawing.Size(164, 63);
+            this.rootElementButton.TabIndex = 5;
+            this.rootElementButton.Text = "Root Element";
+            this.rootElementButton.UseVisualStyleBackColor = true;
+            // 
+            // baldoLocalButton
+            // 
+            this.baldoLocalButton.Location = new System.Drawing.Point(176, 114);
+            this.baldoLocalButton.Name = "baldoLocalButton";
+            this.baldoLocalButton.Size = new System.Drawing.Size(164, 63);
+            this.baldoLocalButton.TabIndex = 6;
+            this.baldoLocalButton.Text = "BaldoLocal";
+            this.baldoLocalButton.UseVisualStyleBackColor = true;
+            // 
+            // logFolderButton
+            // 
+            this.logFolderButton.Location = new System.Drawing.Point(17, 47);
+            this.logFolderButton.Name = "logFolderButton";
+            this.logFolderButton.Size = new System.Drawing.Size(164, 63);
+            this.logFolderButton.TabIndex = 7;
+            this.logFolderButton.Text = "Log Folder";
+            this.logFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // apciFolderButton
+            // 
+            this.apciFolderButton.Location = new System.Drawing.Point(357, 47);
+            this.apciFolderButton.Name = "apciFolderButton";
+            this.apciFolderButton.Size = new System.Drawing.Size(164, 63);
+            this.apciFolderButton.TabIndex = 8;
+            this.apciFolderButton.Text = "APCI Folder";
+            this.apciFolderButton.UseVisualStyleBackColor = true;
+            // 
+            // appwizButton
+            // 
+            this.appwizButton.Location = new System.Drawing.Point(22, 47);
+            this.appwizButton.Name = "appwizButton";
+            this.appwizButton.Size = new System.Drawing.Size(164, 63);
+            this.appwizButton.TabIndex = 9;
+            this.appwizButton.Text = "AppWiz";
+            this.appwizButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.fidoStatusIcon);
+            this.groupBox1.Controls.Add(this.clupStatusIcon);
+            this.groupBox1.Controls.Add(this.serviceStatusIcon);
+            this.groupBox1.Controls.Add(this.fidoButton);
+            this.groupBox1.Controls.Add(this.clupButton);
+            this.groupBox1.Controls.Add(this.bafButton);
+            this.groupBox1.Location = new System.Drawing.Point(10, 213);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(554, 240);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TechTool Services";
+            // 
+            // fidoStatusIcon
+            // 
+            this.fidoStatusIcon.Location = new System.Drawing.Point(383, 62);
+            this.fidoStatusIcon.Name = "fidoStatusIcon";
+            this.fidoStatusIcon.Size = new System.Drawing.Size(93, 83);
+            this.fidoStatusIcon.TabIndex = 5;
+            this.fidoStatusIcon.TabStop = false;
+            // 
+            // clupStatusIcon
+            // 
+            this.clupStatusIcon.Location = new System.Drawing.Point(210, 62);
+            this.clupStatusIcon.Name = "clupStatusIcon";
+            this.clupStatusIcon.Size = new System.Drawing.Size(93, 83);
+            this.clupStatusIcon.TabIndex = 4;
+            this.clupStatusIcon.TabStop = false;
+            // 
+            // serviceStatusIcon
+            // 
+            this.serviceStatusIcon.Location = new System.Drawing.Point(44, 62);
+            this.serviceStatusIcon.Name = "serviceStatusIcon";
+            this.serviceStatusIcon.Size = new System.Drawing.Size(93, 83);
+            this.serviceStatusIcon.TabIndex = 3;
+            this.serviceStatusIcon.TabStop = false;
+            // 
+            // fidoButton
+            // 
+            this.fidoButton.Location = new System.Drawing.Point(349, 151);
+            this.fidoButton.Name = "fidoButton";
+            this.fidoButton.Size = new System.Drawing.Size(164, 63);
+            this.fidoButton.TabIndex = 2;
+            this.fidoButton.Text = "FIDO";
+            this.fidoButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.baldoLocalButton);
+            this.groupBox2.Controls.Add(this.rootElementButton);
+            this.groupBox2.Controls.Add(this.wafButton);
+            this.groupBox2.Controls.Add(this.apciFixButton);
+            this.groupBox2.Location = new System.Drawing.Point(10, 459);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(554, 192);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "TechTool Fixes";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.apciFolderButton);
+            this.groupBox3.Controls.Add(this.logFolderButton);
+            this.groupBox3.Location = new System.Drawing.Point(15, 664);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(548, 130);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "TechTool Folders";
+            // 
+            // servicesButton
+            // 
+            this.servicesButton.Location = new System.Drawing.Point(192, 47);
+            this.servicesButton.Name = "servicesButton";
+            this.servicesButton.Size = new System.Drawing.Size(164, 63);
+            this.servicesButton.TabIndex = 13;
+            this.servicesButton.Text = "Services";
+            this.servicesButton.UseVisualStyleBackColor = true;
+            // 
+            // systemInfoButton
+            // 
+            this.systemInfoButton.Location = new System.Drawing.Point(362, 47);
+            this.systemInfoButton.Name = "systemInfoButton";
+            this.systemInfoButton.Size = new System.Drawing.Size(164, 63);
+            this.systemInfoButton.TabIndex = 14;
+            this.systemInfoButton.Text = "System Info";
+            this.systemInfoButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.systemInfoButton);
+            this.groupBox4.Controls.Add(this.servicesButton);
+            this.groupBox4.Controls.Add(this.appwizButton);
+            this.groupBox4.Location = new System.Drawing.Point(10, 818);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(553, 131);
+            this.groupBox4.TabIndex = 15;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "System Components";
+            // 
+            // QuickSupportLite
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1024, 969);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "QuickSupportLite";
+            this.Text = "QuickSupport Lite";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.fidoStatusIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clupStatusIcon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.serviceStatusIcon)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bafButton;
+        private System.Windows.Forms.Button clupButton;
+        private System.Windows.Forms.Button apciFixButton;
+        private System.Windows.Forms.Button wafButton;
+        private System.Windows.Forms.Button rootElementButton;
+        private System.Windows.Forms.Button baldoLocalButton;
+        private System.Windows.Forms.Button logFolderButton;
+        private System.Windows.Forms.Button apciFolderButton;
+        private System.Windows.Forms.Button appwizButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox fidoStatusIcon;
+        private System.Windows.Forms.PictureBox clupStatusIcon;
+        private System.Windows.Forms.PictureBox serviceStatusIcon;
+        private System.Windows.Forms.Button fidoButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button servicesButton;
+        private System.Windows.Forms.Button systemInfoButton;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Timer serviceStatusTimer;
+        private System.Windows.Forms.Timer clupStatusTimer;
+        private System.Windows.Forms.Timer fidoStatusTimer;
+    }
+}
+
